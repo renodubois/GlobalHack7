3da2018443ab5e9be280cf6eafca40f88559d6ca
robertlandis <robertwlandis@gmail.com>
2018-10-13 02:17:44 -0500

    better inbox of messages

--- src/views/message/Inbox.tsx
+++ src/views/message/Inbox.tsx
@@ -1,19 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- */
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import _ from 'underscore';
+import moment from 'moment-timezone';
+
+import { StyleSheet, Text, View } from 'react-native';
+import FakeConversationData from './fake_conversation_data';
+import ConversationSummary from './ConversationSummary';
+
+import {Message} from '../../types/Message';
 
 type Props = {};
-export default class Inbox extends React.Component<Props> {
+export default class Inbox extends React.Component<Props> {    
     render() {
+        if(_.size(FakeConversationData) == 0){
+            return (
+                <View style={styles.container}>
+                    <Text style={styles.welcome}>You have no conversations!</Text>
+                </View>
+            );
+        }
+
+        const GroupedData = _.chain(FakeConversationData).groupBy('ConversationId').value();
+        let MostRecentConversations: any = {};
+
+        _.each(GroupedData, (Messages, ConversationId: number) => {
+            MostRecentConversations[ConversationId] = _.max(Messages, (Message) => { return moment(Message.DateSent).unix() });
+        });
+
+        const SortedData = _.sortBy(MostRecentConversations, (Message: Message) => { return Message.DateSent; }).reverse();
+
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>Welcome to React Native!</Text>
-                <Text style={styles.instructions}>To get started, edit App.js</Text>
-                <Text style={styles.instructions}>{instructions}</Text>
+                {_.map(SortedData, (Message: Message) => {
+                    return <ConversationSummary key={Message.ConversationId} Messages={GroupedData[Message.ConversationId]} ConversationId={Message.ConversationId} />;
+                })}                    
             </View>
         );
     }
@@ -30,10 +49,5 @@ const styles = StyleSheet.create({
         fontSize: 20,
         textAlign: 'center',
         margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
+    }
 });
