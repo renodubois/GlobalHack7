0a4868ee3dd6dba332452a54bd5335e5b7b2ccbc
robertlandis <robertwlandis@gmail.com>
2018-10-13 05:30:29 -0500

    let's try making our own sidebar

--- src/views/Router.ts
+++ src/views/Router.ts
@@ -1,13 +1,19 @@
-import { DrawerNavigator } from 'react-navigation'
+import { DrawerNavigator } from 'react-navigation';
 
-import HomeComponent from './HomeComponent'
-import Inbox from './message/Inbox'
+import HomeComponent from './HomeComponent';
+import Inbox from './message/Inbox';
+import Conversation from './message/Conversation';
+import SideBar from './SideBar';
 
 const NavOptions = {
     initialRouteName: 'Home',
+    contentComponent: SideBar
 };
 
 export default DrawerNavigator({
-    Home: { screen: HomeComponent },
-    History: { screen: Inbox },
+    'Home': { screen: HomeComponent },
+    'Current': { screen: Conversation },
+    'History': { screen: Inbox },
+    'Conversation': { screen: Conversation },
+    'SideBar': { screen: SideBar }
 }, NavOptions);
