2c2fa8a081fdece133ff01fc31dd6aafd8b6154b
robertlandis <robertwlandis@gmail.com>
2018-10-13 04:14:56 -0500

    add fake user data

---
+++ src/stores/users.ts
@@ -0,0 +1,17 @@
+export default {
+    1: {
+        UserId: 1,
+        Name: "H.H. Host",
+        IsHost: true
+    },
+    2: {
+        UserId: 2,
+        Name: "Ima Guest",
+        IsHost: false
+    },
+    3: {
+        UserId: 3,
+        Name: "Bette R. Host",
+        IsHost: true
+    }
+};
