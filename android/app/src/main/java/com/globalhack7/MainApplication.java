8edb0f64fbfd24bb918c5379947568a6917ba8eb
robertlandis <robertwlandis@gmail.com>
2018-10-13 16:05:11 -0500

    install audio recording packages

--- android/app/src/main/java/com/globalhack7/MainApplication.java
+++ android/app/src/main/java/com/globalhack7/MainApplication.java
@@ -3,10 +3,12 @@ package com.globalhack7;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnim.rn.audio.ReactNativeAudioPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.futurice.rctaudiotoolkit.AudioPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new ReactNativeAudioPackage(),
+          new AudioPackage()
       );
     }
 
