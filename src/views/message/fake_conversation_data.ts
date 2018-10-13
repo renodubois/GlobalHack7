28c9dd5dac9b41013a5e147f85d1854b886aeb97
robertlandis <robertwlandis@gmail.com>
2018-10-13 02:17:02 -0500

    fake conversation data

---
+++ src/views/message/fake_conversation_data.ts
@@ -0,0 +1,23 @@
+export default [
+    {
+        ConversationId: 1234,
+        MessageId: 3438,
+        FromUserId: 1,
+        DateSent: "2018-02-02T00:39:00Z",
+        Text: "I would like to buy a hamburger."
+    },
+    {
+        ConversationId: 1234,
+        MessageId: 39847,
+        FromUserId: 2,
+        DateSent: "2018-02-23T11:38:53Z",
+        Text: "I have a hamburger for you."
+    },
+    {
+        ConversationId: 95043,
+        MessageId: 34802,
+        FromUserId: 1,
+        DateSent: "2018-03-04T12:43:20Z",
+        Text: "Can you recommend a school for my kids?"
+    }
+];
